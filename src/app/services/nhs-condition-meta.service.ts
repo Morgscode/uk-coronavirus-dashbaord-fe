6322d37d29695d75f2267937aa1c5ab75cf3dc11
Luke Morgan <morgan.luke94@gmail.com>
2020-05-24 13:27:48 +0100

    puts app in production mode

--- src/app/services/nhs-condition-meta.service.ts
+++ src/app/services/nhs-condition-meta.service.ts
@@ -7,7 +7,7 @@ import { NHSConditionApiMeta } from '../models/NHSConditionApiMeta';
   providedIn: 'root'
 })
 export class NHSConditionMetaService {
-  env: string = 'dev';
+  env: string = 'pro';
   covidInfoBaseUrl: string;
   conditionString: string = 'nhs-covid-19';
   
