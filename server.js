06892f8898f6be288c9a817353d7edd0db5938eb
Luke Morgan <morgan.luke94@gmail.com>
2020-05-24 09:02:36 +0100

    adds gzip extenstion to server.js directive

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/uk-covid-dashboard/browser'));
 // Start the app by listening on the default
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/uk-covid-dashboard/browser/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/uk-covid-dashboard/browser/index.html.gz'));
 });
 // Heroku port
 app.listen(process.env.PORT || 4200);
