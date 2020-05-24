e21ac03cf78eb15d6727fe9082e8a245b2efc654
Luke Morgan <morgan.luke94@gmail.com>
2020-05-24 09:13:40 +0100

    adds compression to server script

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const path = require('path');
+const compression = require('compression');
 const app = express();
+
+//compress all responses
+app.use(compression());
 // Run the app by serving the static files in the dist directory
 app.use(express.static(__dirname + '/dist/uk-covid-dashboard/browser'));
 // Start the app by listening on the default
