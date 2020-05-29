dbe01d26d272fe5a88b90a0a870b0f2eb97a590e
Luke Morgan <morgan.luke94@gmail.com>
2020-05-29 02:22:56 +0100

    pippes numbers, adds font awesome locally

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ app.use(compression());
 // Run the app by serving the static files in the dist directory
 app.use(express.static(__dirname + '/dist/uk-covid-dashboard/browser'));
 // route all get requests through index.html
-app.get('/*', function(req, res) {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/uk-covid-dashboard/browser/index.html'));
 });
 // Heroku port
+console.log(process.env.PORT);
 app.listen(process.env.PORT || 4200);
