1fb52239575481601c8f92d7283707751e08f88d
Luke Morgan <morgan.luke94@gmail.com>
2020-06-01 21:37:09 +0100

    tidies app module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { FormsModule } from "@angular/forms";
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { ChartsModule } from 'ng2-charts';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
