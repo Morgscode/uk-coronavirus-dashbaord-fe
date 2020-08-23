46b2f60972b10b7d495d86856b16e06c30d9aca9
Luke Morgan <morgan.luke94@gmail.com>
2020-08-23 15:05:02 +0100

    responsive improvements

--- src/app/services/nhs-condition-meta.service.ts
+++ src/app/services/nhs-condition-meta.service.ts
@@ -1,23 +1,27 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'; 
-import { Observable } from 'rxjs';
-import { NHSConditionApiMeta } from '../models/NHSConditionApiMeta';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { NHSConditionApiMeta } from "../models/NHSConditionApiMeta";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
-
 export class NHSConditionMetaService {
-  env: string = 'prod';
+  env: string = "dev";
   covidInfoBaseUrl: string;
-  conditionString: string = 'nhs-covid-19';
-  
+  conditionString: string = "nhs-covid-19";
+
   constructor(private http: HttpClient) {
-    (this.env == 'dev') ? this.covidInfoBaseUrl = 'http://127.0.0.1:8888/uk-coronavirus-dashboard-api/public/info/' : this.covidInfoBaseUrl = 'https://api.ukcoronavirusstatistics.com/info/';
+    this.env == "dev"
+      ? (this.covidInfoBaseUrl =
+          "http://127.0.0.1:8888/uk-coronavirus-dashboard-api/public/info/")
+      : (this.covidInfoBaseUrl =
+          "https://api.ukcoronavirusstatistics.com/info/");
   }
 
   public getNHSConditionApiMeta(): Observable<NHSConditionApiMeta> {
-    return this.http.get<NHSConditionApiMeta>(`${this.covidInfoBaseUrl}${this.conditionString}`);
+    return this.http.get<NHSConditionApiMeta>(
+      `${this.covidInfoBaseUrl}${this.conditionString}`
+    );
   }
-
 }
