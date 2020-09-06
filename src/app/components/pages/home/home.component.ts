e7b40fde0c56c665080a2028de0afa5af25c9e70
Luke Morgan <morgan.luke94@gmail.com>
2020-09-06 11:31:06 +0100

    moves all page meta setup into public method on each page

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,21 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { Title }     from '@angular/platform-browser';
-import { Meta } from '@angular/platform-browser';
+import { Component, OnInit } from "@angular/core";
+import { Title } from "@angular/platform-browser";
+import { Meta } from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
- 
-  constructor(private titleService: Title, private meta: Meta) { }
+  constructor(private titleService: Title, private meta: Meta) {}
 
   ngOnInit() {
-    this.titleService.setTitle( 'Track UK Coronavirus Statistics' );
-    this.meta.updateTag(
-      { name: 'description', content: 'An Independant UK Coronavirus tracking dashboard. Information held within has been made publicly available by Public Health England, The Department for Health and Social Care and the NHS.' },
-    );
+    this.setupPageMeta();
   }
 
+  public setupPageMeta() {
+    this.titleService.setTitle("Track UK Coronavirus Statistics");
+    this.meta.updateTag({
+      name: "description",
+      content:
+        "An Independant UK Coronavirus tracking dashboard. Information held within has been made publicly available by Public Health England, The Department for Health and Social Care and the NHS.",
+    });
+  }
 }
